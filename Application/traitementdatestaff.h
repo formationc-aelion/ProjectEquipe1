e9d9009d8585a384c7057be3a0b20025e3300c21
Storvikson <47659513+Storvikson@users.noreply.github.com>
2019-03-13 16:20:48 +0100

    Création des methode de traitement de data pour le staff

--- Application/traitementdatestaff.h
+++ Application/traitementdatestaff.h
@@ -18,7 +18,7 @@ QSortFilterProxyModel* CreateSortingModelStaff (QObject *ui,QSqlTableModel *Staf
 void AjoutStaff(Staff staffAdd,QSqlTableModel *StaffModel );
 void DeleteStaff(QSortFilterProxyModel *StaffSortingModel,QModelIndex IndexASuppr,QSqlTableModel *StaffModel);
 
-void modificationStaff(Film staffAdd,QSqlTableModel *StaffModel,QSortFilterProxyModel *StaffSortingModel,QModelIndex IndexAModifier);
+void modificationStaff(Staff staffAdd,QSqlTableModel *StaffModel,QSortFilterProxyModel *StaffSortingModel,QModelIndex IndexAModifier);
 
 
 #endif // TRAITEMENTDATESTAFF_H
