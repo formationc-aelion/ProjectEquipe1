3fbeb8a703921c916a1a8f00bfb1209e5c3b1c74
Gaït <marguerite.vassallo@gmail.com>
2019-03-13 15:03:52 +0100

    infos manquantes dans test film

--- test_google/tst_testconstructor.h
+++ test_google/tst_testconstructor.h
@@ -6,24 +6,6 @@
 
 using namespace testing;
 
-TEST(TestFilmConstructor, NewOne)
-{
-    //given
-        QString titre = "Terminator";
-        QString genre = "Science-Fiction";
-        QString langue = "Anglais";
-        int annee = 1990;
-        int duree = 120;
-    //when
-        Film Terminator (titre,genre,langue,annee,duree);
-    //then
-        EXPECT_EQ(Terminator.titre(), titre);
-        EXPECT_EQ(Terminator.genre(), genre);
-        EXPECT_EQ(Terminator.langue(), langue);
-        EXPECT_EQ(Terminator.annee(), annee);
-        EXPECT_EQ(Terminator.duree(), duree);
-}
-
 TEST(TestFilmConstructor, Default)
 {
     //given
@@ -35,4 +17,50 @@ TEST(TestFilmConstructor, Default)
         EXPECT_EQ(Terminator.langue(), "langue");
         EXPECT_EQ(Terminator.annee(), 0);
         EXPECT_EQ(Terminator.duree(), 0);
+        EXPECT_EQ(Terminator.photo(), QByteArray());
+        EXPECT_EQ(Terminator.info(), "");
 }
+
+TEST(TestFilmConstructor, NewOne)
+{
+    //given
+        QString titre = "Terminator";
+        QString genre = "Science-Fiction";
+        QString langue = "Anglais";
+        int annee = 1990;
+        int duree = 120;
+
+    //when
+        Film Terminator (titre,genre,langue,annee,duree);
+    //then
+        EXPECT_EQ(Terminator.titre(), titre);
+        EXPECT_EQ(Terminator.genre(), genre);
+        EXPECT_EQ(Terminator.langue(), langue);
+        EXPECT_EQ(Terminator.annee(), annee);
+        EXPECT_EQ(Terminator.duree(), duree);       
+}
+
+TEST(TestFilmConstructor, Complet)
+{
+    //given
+        QString titre = "Terminator";
+        QString genre = "Science-Fiction";
+        QString langue = "Anglais";
+        int annee = 1990;
+        int duree = 120;
+        QByteArray photo = QByteArray();
+        QString info = "";
+
+    //when
+        Film Terminator (titre,genre,langue,annee,duree);
+    //then
+        EXPECT_EQ(Terminator.titre(), titre);
+        EXPECT_EQ(Terminator.genre(), genre);
+        EXPECT_EQ(Terminator.langue(), langue);
+        EXPECT_EQ(Terminator.annee(), annee);
+        EXPECT_EQ(Terminator.duree(), duree);
+        EXPECT_EQ(Terminator.photo(), photo);
+        EXPECT_EQ(Terminator.info(), info);
+}
+
+
