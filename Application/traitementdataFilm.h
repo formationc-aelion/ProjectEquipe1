fa9472be9601b9a7a3d6cb533115ff0a1aeef656
Storvikson <47659513+Storvikson@users.noreply.github.com>
2019-03-13 13:53:44 +0100

    Ajout de la creation du model pour le staff dans le bon fichier

--- Application/traitementdataFilm.h
+++ Application/traitementdataFilm.h
@@ -35,28 +35,7 @@ mFilmSortingModel->setSourceModel(FilmModel);
 return mFilmSortingModel;
 }
 
-QSqlTableModel* CreateModelStaff (QObject *ui)
-{
-QSqlDatabase db = QSqlDatabase::database("connexionBDDfilm");
-QSqlTableModel *mStaffModel = new QSqlTableModel (ui,db);
-mStaffModel->setTable("staff");
-mStaffModel->select();
-mStaffModel->setEditStrategy(QSqlTableModel::OnManualSubmit);
 
-return mStaffModel;
-}
-
-QSortFilterProxyModel* CreateSortingModelStaff (QObject *ui,QSqlTableModel *StaffModel)
-{
-QSqlDatabase db = QSqlDatabase::database("connexionBDDfilm");
-QSortFilterProxyModel *mStaffSortingModel = new QSortFilterProxyModel(ui);
-mStaffSortingModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
-mStaffSortingModel->setSortCaseSensitivity(Qt::CaseInsensitive);
-mStaffSortingModel->sort(1, Qt::AscendingOrder);
-mStaffSortingModel->setDynamicSortFilter(true);
-mStaffSortingModel->setSourceModel(StaffModel);
-return mStaffSortingModel;
-}
 
 int Conversionint(QString DureeFilm){
     QString conversion_galere = DureeFilm;
--- Application/traitementdatestaff.h
+++ Application/traitementdatestaff.h
@@ -1,4 +1,37 @@
 #ifndef TRAITEMENTDATESTAFF_H
 #define TRAITEMENTDATESTAFF_H
+#include <QMainWindow>
+#include"film.h"
+#include "Staff.h"
+#include<QDebug>
+#include <QVariant>
+#include <QSqlRecord>
+#include <QSqlDatabase>
+#include <QSqlTableModel>
+#include <QSortFilterProxyModel>
+#include <mainwindow.h>
+
+QSqlTableModel* CreateModelStaff (QObject *ui)
+{
+QSqlDatabase db = QSqlDatabase::database("connexionBDDfilm");
+QSqlTableModel *mStaffModel = new QSqlTableModel (ui,db);
+mStaffModel->setTable("staff");
+mStaffModel->select();
+mStaffModel->setEditStrategy(QSqlTableModel::OnManualSubmit);
+
+return mStaffModel;
+}
+
+QSortFilterProxyModel* CreateSortingModelStaff (QObject *ui,QSqlTableModel *StaffModel)
+{
+QSqlDatabase db = QSqlDatabase::database("connexionBDDfilm");
+QSortFilterProxyModel *mStaffSortingModel = new QSortFilterProxyModel(ui);
+mStaffSortingModel->setFilterCaseSensitivity(Qt::CaseInsensitive);
+mStaffSortingModel->setSortCaseSensitivity(Qt::CaseInsensitive);
+mStaffSortingModel->sort(1, Qt::AscendingOrder);
+mStaffSortingModel->setDynamicSortFilter(true);
+mStaffSortingModel->setSourceModel(StaffModel);
+return mStaffSortingModel;
+}
 
 #endif // TRAITEMENTDATESTAFF_H
